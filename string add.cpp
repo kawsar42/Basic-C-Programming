457ffd4839179c969d1a3fafef7df44484c90249
Kawsar_hossain <59926691+kawsar42@users.noreply.github.com>
2024-11-25 22:17:40 +0600

    Add files via upload

---
+++ string add.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+     char ch;
+     cin >> ch;
+     char c=ch+32;
+     cout<<  c<< endl;
+}
+int main()
+{
+    solve();
+    return 0;
+}
